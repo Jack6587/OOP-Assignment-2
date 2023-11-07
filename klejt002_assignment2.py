d3dc39cf525f0efa57cacd0ec5c07f73f8a00e0f
Jack <klejt002@mymail.unisa.edu.au>
2023-11-08 00:43:56 +1030

    Added details to Alchemist And Laboratory classes

--- klejt002_assignment2.py
+++ klejt002_assignment2.py
@@ -1,5 +1,22 @@
+class Alchemist:
+    def __init__(self, attack, strength, defense, magic, ranged, necromancy, laboratory):
+        self.attack = attack
+        self.strength = strength
+        self.defense = defense
+        self.magic = magic
+        self.ranged = ranged
+        self.necromancy = necromancy
+        self.laboratory = laboratory
+        self.recipes = {}
+
 class Laboratory:
     def __init__(self):
         self.potions = []
         self.herbs = []
         self.catelysts = []
+
+    def mixPotion(self):
+        pass
+
+    def addReagent(self):
+        pass
