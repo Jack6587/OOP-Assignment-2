68db33a3b806be8c9d10bf258bf4cd4bb7799562
Jack <klejt002@mymail.unisa.edu.au>
2023-11-08 14:41:02 +1030

    added the shell for all classes, started the process of inheritance and just defined methods but have not included anything in them

--- klejt002_assignment2.py
+++ klejt002_assignment2.py
@@ -1,20 +1,40 @@
+from abc import ABC, abstractmethod
+
 class Alchemist:
     def __init__(self, attack, strength, defense, magic, ranged, necromancy, laboratory):
-        self.attack = attack
-        self.strength = strength
-        self.defense = defense
-        self.magic = magic
-        self.ranged = ranged
-        self.necromancy = necromancy
-        self.laboratory = laboratory
-        self.recipes = {}
+        self.__attack = attack
+        self.__strength = strength
+        self.__defense = defense
+        self.__magic = magic
+        self.__ranged = ranged
+        self.__necromancy = necromancy
+        self.__laboratory = laboratory
+        self.__recipes = {}
+
+    def getLaboratory(self):
+        pass
+    
+    def getRecipes(self):
+        return self.__recipes
+
+    def mixPotion(self):
+        pass
+
+    def drinkPotion(self):
+        pass
+
+    def collectReagent(self):
+        pass
+
+    def refineReagents(self):
+        pass
 
 
 class Laboratory:
     def __init__(self):
-        self.potions = []
-        self.herbs = []
-        self.catelysts = []
+        self.__potions = []
+        self.__herbs = []
+        self.__catalysts = []
 
     def mixPotion(self):
         pass
@@ -24,12 +44,86 @@ class Laboratory:
 
 class Potion:
     def __init__(self, name, stat, boost):
-        self.name = name
-        self.stat = stat
-        self.boost = boost
+        self.__name = name
+        self.__stat = stat
+        self.__boost = boost
+
+    @abstractmethod
+    def calculateBoost(self):
+        pass
+
+    def getName(self):
+        return self.__name
+
+    def getStat(self):
+        return self.__stat
+    
+    def getBoost(self):
+        return self.__boost
+
+    def setBoost(self):
+        pass
+
+class SuperPotion(Potion):
+    def __init__(self, name, stat, boost, herb, catalyst):
+        super().__init__(name, stat, boost)
+        self.__herb = herb
+        self.__catalyst = catalyst
+
+class ExtremePotion(Potion):
+    def __init__(self, name, stat, boost, reagent, potion):
+        super().__init__(name, stat, boost)
+        self.__reagent = reagent
+        self.__potion = potion
+
+    def calculateBoost(self):
+        pass
+    
+    def getReagent(self):
+        pass
+
+    def getPotion(self):
+        return self.__potion
 
 class Reagent:
     def __init__(self, name, potency):
-        self.name = name
-        self.potency = potency
+        self.__name = name
+        self.__potency = potency
 
+    @abstractmethod
+    def refine(self):
+        pass
+
+    def getName(self):
+        return self.__name
+
+    def getPotency(self):
+        return self.__potency
+    
+    def setPotency(self):
+        pass
+
+class Herb(Reagent):
+    def __init__(self, name, potency, grimy):
+        super().__init__(name, potency)
+        self.__grimy = grimy
+
+    def refine(self):
+        pass
+
+    def getGrimy(self):
+        return self.__grimy
+    
+    def setGrimy(self):
+        pass
+
+class Catalyst(Reagent):
+    def __init__(self, name, potency, quality):
+        super().__init__(name, potency)
+        self.__quality = quality
+
+    def refine(self):
+        pass
+
+    def getQuality(self):
+        return self.__quality
